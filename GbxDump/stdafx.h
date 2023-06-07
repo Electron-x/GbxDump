78aa2ab4750e670e0246a7b91e6ab17d61326954
Electron <electron@freakmail.de>
2023-06-07 17:48:54 +0200

    Removed definition of _CRT_NON_CONFORMING_SWPRINTFS

--- GbxDump/stdafx.h
+++ GbxDump/stdafx.h
@@ -31,7 +31,6 @@
 #define WIN32_LEAN_AND_MEAN		// Exclude rarely-used stuff from Windows headers
 
 #define _CRT_SECURE_NO_WARNINGS
-#define _CRT_NON_CONFORMING_SWPRINTFS
 
 #ifndef STRICT
 #define STRICT
