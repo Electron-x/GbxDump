912eec1b78acf91a3b89f67550c30a76205bd9c8
Electron <electron@freakmail.de>
2023-08-27 19:44:33 +0200

    Additional symbol definitions for the context menu

--- GbxDump/resource.h
+++ GbxDump/resource.h
@@ -3,9 +3,11 @@
 // Used by GbxDump.rc
 //
 #define IDI_GBXDUMP                     101
-#define IDR_THUMB                       102
-#define IDR_GER_POPUP                   103
-#define IDR_ENG_POPUP                   104
+#define IDB_THUMB                       102
+#define IDR_GER_POPUP_DIALOG            103
+#define IDR_ENG_POPUP_DIALOG            104
+#define IDR_GER_POPUP_THUMB             105
+#define IDR_ENG_POPUP_THUMB             106
 #define IDD_GER_GBXDUMP                 400
 #define IDS_GER_FILTER_GBX              401
 #define IDS_GER_FILTER_PNG              402
@@ -59,7 +61,7 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
-#define _APS_NEXT_RESOURCE_VALUE        105
+#define _APS_NEXT_RESOURCE_VALUE        107
 #define _APS_NEXT_COMMAND_VALUE         32773
 #define _APS_NEXT_CONTROL_VALUE         1009
 #define _APS_NEXT_SYMED_VALUE           101
